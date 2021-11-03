d41562b5e986fba67946839a1b9447958c4832d7
Daniel Lee <daniellee@Daniels-MacBook-Air.local>
2021-11-02 18:13:49 -0700

    added new component/minor css

--- project-2-react/src/Components/RandomAnime/RandomAnime.js
+++ project-2-react/src/Components/RandomAnime/RandomAnime.js
@@ -1,33 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
 const RandomAnime = () => {
-const [anime, setRandomAnime] = useState([]);
+	const [anime, setRandomAnime] = useState([]);
+	const [addAnime, SetAddAnime] = useState([]);
 
-const handleRandomClick = () => {
-	fetch(
-		`https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1`
-	)
-		.then((response) => response.json())
-		.then((data) => setRandomAnime(data.results.slice(0, 1)));
-};
-const animeJSX = anime.map((anime) => {
-	return (
-		<div>
-			<img src={anime.image_url} alt={anime.rank} /> <p>{anime.title}</p><p>{anime.synopsis}</p>
-		</div>
-	);
-});
+	const handleRandomClick = () => {
+		fetch(
+			`https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data)
+				setRandomAnime(data.results)
+			});
+		};
+
+const addAnimeToList = () => {
+		console.log(addAnime)
+		SetAddAnime(addAnime);
+	};
 
 
-
-
-    return (
-			<div className='RandomAnime'>
-				<button onClick={handleRandomClick}>Random Anime</button>
-                <button>Add to watch list</button>
-				{animeJSX}
+	const animeJSX = anime.map((anime) => {
+		return (
+			<div>
+				<img src={anime.image_url} alt={anime.rank} /> <p>{anime.title}</p>
+				<p>{anime.synopsis}</p>
+				<button onClick = {addAnimeToList}>Add to Watch List</button>
 			</div>
 		);
-}
+	});
+
+	
+
+	return (
+		<div className='RandomAnime'>
+			<button onClick={handleRandomClick}>Random Anime</button>
+			{animeJSX}
+
+		</div>
+	);
+};
 
 export default RandomAnime;
---
+++ project-2-react/src/Components/WatchList/WatchList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WatchList = () => {
+    return (
+        <div className = "WatchList"></div>
+    )
+}
+
+export default WatchList;
